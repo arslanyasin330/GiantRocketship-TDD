2a16c4b9e10b64dfd94c215fcc06c9dc3a2730cb
Muhammad Arslan <arslanyasin330@users.noreply.github.com>
2024-11-17 22:50:11 +0500

    Contact Us

--- src/pageObjects/contactUs/contactUs.po.ts
+++ src/pageObjects/contactUs/contactUs.po.ts
@@ -15,14 +15,24 @@ export class ContactUs extends BasePage {
         return this.page.locator(this.formHeading).first();
     }
 
+    /**
+     * Retrieves the first frame locator for the "Contact Us" form iframe.
+     * @returns {Promise<FrameLocator>} - A promise that resolves to a FrameLocator object,
+     * allowing interaction with the content of the "Contact Us" form iframe.
+     */
     async contactUsFrameContent(): Promise<FrameLocator> {
         return this.page.frameLocator(this.contactFormFrame).first();
     }
-
+     
     async pageTitle(): Promise<Locator> {
         return this.page.locator(this.pageHeading).first();
     }
-
+    
+    /**
+     * Creates an instance of the `ContactForm` class for interacting with the "Contact Us" form.
+     * @returns {Promise<ContactForm>} - A promise that resolves to an instance of the `ContactForm` class,
+     * providing methods to interact with the "Contact Us" form.
+     */
     async contactUsForm(): Promise<ContactForm> {
         const contactForm = await this.contactUsFrameContent();
         return new ContactForm(this.page, contactForm);
